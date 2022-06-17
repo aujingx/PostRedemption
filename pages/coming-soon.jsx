1de6d7fce1309e5bfd25c9255dc96637562b78d4
kuannnn <kuanthedev@gmail.com>
2022-06-18 04:54:28 +0800

    bugfix/escape html

--- pages/coming-soon.jsx
+++ pages/coming-soon.jsx
@@ -13,13 +13,14 @@ const ComingSoon = () => {
       <div className="mt-10 w-[800px] h-auto text-xl leading-10">
         <p>
           After humans have ravaged the earth for thousands of years, they have
-          finally succeeded in "transforming" the earth into a barren land:
-          nuclear pollution, plastic waste, electronic fog, dust smog ... All
-          the pollution crises you can think of are in this land. In this game
-          you will play as a robot produced by the D.U.S.T -- dangerous union of
-          science & technology, which were cast before the last humans left the
-          earth, and in the vast years after humans left, you still faithfully
-          perform your most core and most original work: resource recycling
+          finally succeeded in &quot;transforming&quot; the earth into a barren
+          land: nuclear pollution, plastic waste, electronic fog, dust smog ...
+          All the pollution crises you can think of are in this land. In this
+          game you will play as a robot produced by the D.U.S.T -- dangerous
+          union of science &amp; technology, which were cast before the last
+          humans left the earth, and in the vast years after humans left, you
+          still faithfully perform your most core and most original work:
+          resource recycling
         </p>
       </div>
       <Image src={loading} alt="" />
